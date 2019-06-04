e1aee2e636df640189b6a2c6f8e4cc527bca7bcd
HelloWorldTian <382419872@qq.com>
2019-06-04 23:27:21 +0800

    增加DoTween动画
    UI 增加DoTween动画

--- Assets/Scripts/UIMenuPanel.cs
+++ Assets/Scripts/UIMenuPanel.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using DG.Tweening;
 public class UIMenuPanel : MonoBehaviour {
 
     public GameObject firstPanel;
@@ -17,10 +17,11 @@ public class UIMenuPanel : MonoBehaviour {
 
     public Button firstCloseBtn;
     public Button secondCloseBtn;
+
+    private float tweenTime = 0.2f;
     // Use this for initialization
     void Start () {
         AddEventListener();
-
     }
 	
 	// Update is called once per frame
@@ -65,15 +66,34 @@ public class UIMenuPanel : MonoBehaviour {
     {
         //增加动画
         Debug.Log("close1");
-        gameObject.SetActive(false);
+        Tweener tweener = firstPanel.transform.DOScaleY(0, tweenTime);
+        tweener.SetEase(Ease.Linear);
+        tweener.OnComplete(() =>
+        {
+            Debug.Log("123");
+            gameObject.SetActive(false);
+        });
+
     }
     void CloseSecondPanel()
     {
         Debug.Log("close2");
         sencondBtnReset();
         //判断是否播放视频中
-        secondPanel.SetActive(false);
-        firstPanel.SetActive(true);
+
+ 
+        Tweener tweener = secondPanel.transform.DOScaleY(0, tweenTime);
+        tweener.SetEase(Ease.Linear);
+        tweener.Play();//动画播放
+        tweener.OnComplete(() =>
+        {
+            secondPanel.SetActive(false);
+            firstPanel.SetActive(true);
+            tweener = firstPanel.transform.DOScaleY(1, tweenTime);
+            tweener.SetEase(Ease.Linear);
+            tweener.Play();//动画播放
+        });
+
     }
 
     void firstBtnClickEvent(int index)
@@ -81,15 +101,25 @@ public class UIMenuPanel : MonoBehaviour {
        
         firstBtnReset();
         firstPanelBtns[index].GetComponent<Image>().sprite = BtnClickImg[index];
-        //增加动画
-        firstPanel.SetActive(false);
-        secondBtnClickEvent(index);
+
+  
+        Tweener tweener = firstPanel.transform.DOScaleY(0, tweenTime);
+        tweener.SetEase(Ease.Linear);
+        tweener.OnComplete(() =>
+        {
+            firstPanel.SetActive(false);
+            tweener = secondPanel.transform.DOScaleY(1, tweenTime); 
+            tweener.Play();
+            secondBtnClickEvent(index);
+        });
+
     }
     void secondBtnClickEvent(int index)
     {
        
         sencondBtnReset();
         secondPanel.SetActive(true);
+     
         secondPanelBtns[index].GetComponent<Image>().sprite = BtnClickImg[index];
         secondPanelBtns[index].transform.localScale = new Vector3(1.7f, 1.7f, 1.7f);
     }
@@ -100,5 +130,11 @@ public class UIMenuPanel : MonoBehaviour {
         firstBtnReset();
         gameObject.SetActive(true);
         firstPanel.SetActive(true);
+
+        Tweener tweener = firstPanel.transform.DOScaleY(1, tweenTime);
+        tweener.SetEase(Ease.Linear);
+        tweener.Play();//动画播放
     }
+
+    
 }
